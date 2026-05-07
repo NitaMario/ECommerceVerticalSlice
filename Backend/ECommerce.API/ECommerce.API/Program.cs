834eca4ce2a80fe7b98fc4cf61b9b5d449ee97ea
NitaMario <marionita2664@gmail.com>
2026-05-08 02:48:37 +0300

    ci: added di for for swagger, mediatr, cors and connectionstring

--- Backend/ECommerce.API/ECommerce.API/Program.cs
+++ Backend/ECommerce.API/ECommerce.API/Program.cs
@@ -1,16 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOpenApi();
+
+// Application services
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+    config.RegisterServicesFromAssembly(assembly));
+
+// Infrastracture services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularFrontend",
+        policy => policy
+        .WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
+// Web API services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularFrontend");
+
 app.Run();
 
