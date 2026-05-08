a8abc538feecd5116bfa4c687ce81138b6cbfd41
NitaMario <marionita2664@gmail.com>
2026-05-09 01:24:22 +0300

    ci: add di for jwtbearer, accomodate swagger for jwtbearer

--- Backend/ECommerce.API/ECommerce.API/GlobalUsing.cs
+++ Backend/ECommerce.API/ECommerce.API/GlobalUsing.cs
@@ -3,5 +3,8 @@ global using Carter;
 global using BCrypt.Net;
 global using Microsoft.Data.SqlClient;
 global using System.IdentityModel.Tokens.Jwt;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+global using System.Text;
 global using System.Security.Claims;
 global using ECommerce.API.Infrastracture;
--- Backend/ECommerce.API/ECommerce.API/Program.cs
+++ Backend/ECommerce.API/ECommerce.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,31 @@ var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(assembly));
+
+// Security services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var jwtSettings = builder.Configuration.GetSection("Jwt");
+    var secretKey = jwtSettings["Secret"]!;
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+    };
+});
+builder.Services.AddAuthorization();
 
 // Infrastracture services
 builder.Services.AddCors(options =>
@@ -21,12 +48,42 @@ builder.Services.AddTransient<ISqlConnectionFactory, SqlConnectionFactory>();
 
 // Web API services
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Enter 'Bearer [space] your-token'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapCarter();
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularFrontend");
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -35,14 +92,15 @@ if (app.Environment.IsDevelopment())
     var secret = app.Configuration["Jwt:Secret"];
     if (string.IsNullOrEmpty(secret) || secret == "SuperSecretKeyForEcommerceSliceAppPracticalExercise!@#!")
     {
-        Console.WriteLine("SECURITY NOTE: Using a hardcoded JWT Secret for convenince of testing.");
+        Console.WriteLine("SECURITY NOTE: Using a hardcoded JWT Secret for convenience of testing.");
         Console.WriteLine("In production, this would be managed via Environment Variables.");
     }
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.UseCors("AllowAngularFrontend");
+app.MapCarter();
 
 app.Run();
 
